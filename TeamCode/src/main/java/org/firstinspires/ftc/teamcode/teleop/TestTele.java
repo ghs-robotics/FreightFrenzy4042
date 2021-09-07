57166b38feb1d10f00c935110e660b6f5e82e586
theo <theo@seelye.net>
2021-09-07 16:08:53 -0700

    test teleop opmode for running motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestTele.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.data.FieldPositions;
+import org.firstinspires.ftc.teamcode.robot_components.cv.CVModule;
+import org.firstinspires.ftc.teamcode.robot_components.input.Btn;
+import org.firstinspires.ftc.teamcode.robot_components.input.Controller;
+import org.firstinspires.ftc.teamcode.robot_components.robot.Robot;
+
+@TeleOp(name="TestTele", group="Linear Opmode")
+public class TestTele extends LinearOpMode implements FieldPositions {
+    
+    // Declare OpMode members
+    Robot robot;
+    Controller controller1;
+    Controller controller2;
+
+    @Override
+    public void runOpMode() {
+
+        // Code to run ONCE when the driver hits INIT
+        robot = new Robot(hardwareMap, telemetry); // new CVModule(hardwareMap, telemetry);
+        controller1 = new Controller(gamepad1); // Whoever presses start + a
+        controller2 = new Controller(gamepad2); // Whoever presses start + b
+
+//        robot;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+//        robot.cameras.webcam.pauseViewport();
+//        robot.activateFieldLocalization();
+//        telemetry.setMsTransmissionInterval(20);
+
+        robot.resetGyroAngle();
+        robot.resetElapsedTime();
+
+        while (opModeIsActive()) {
+
+            // Registers controller input
+            controller1.update();
+            controller2.update();
+
+            // -----------------------------------------------------------------------------------------
+            // -----------------------------------------------------------------------------------------
+            // ------------------------------   CONTROLLER 1 FUNCTIONS   -------------------------------
+            // -----------------------------------------------------------------------------------------
+            // -----------------------------------------------------------------------------------------
+            // NOTE: TO USE THESE FUNCTIONS PRESS START A
+            //DRIVER FUNCTIONS
+            if (controller1.right_bumper != Btn.PRESSED) {
+                robot.calculateDrivePowers(
+                        controller1.left_stick_x,
+                        controller1.left_stick_y,
+                        controller1.right_stick_x,
+                        controller1.right_stick_y
+                );
+            }
+
+            // -----------------------------------------------------------------------------------------
+            // -----------------------------------------------------------------------------------------
+            // ------------------------------   CONTROLLER 2 FUNCTIONS   -------------------------------
+            // -----------------------------------------------------------------------------------------
+            // -----------------------------------------------------------------------------------------
+            // NOTE: TO USE THESE FUNCTIONS, PRESS START B
+            //OPERATOR FUNCTIONS
+
+        }
+    }
+}
