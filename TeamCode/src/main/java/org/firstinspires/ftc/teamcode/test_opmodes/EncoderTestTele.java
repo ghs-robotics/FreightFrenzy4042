c8a994315f982107c90b47b3c3a3c4c4f2108734
Simon <SimonNDao12@gmail.com>
2021-09-08 22:25:54 -0700

    moved theo's test opmode to the test opmodes folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_opmodes/EncoderTestTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_opmodes/EncoderTestTele.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleop;
+package org.firstinspires.ftc.teamcode.test_opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
