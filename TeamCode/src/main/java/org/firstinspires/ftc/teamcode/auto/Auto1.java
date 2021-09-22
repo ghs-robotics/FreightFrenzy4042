47aa2dbdeab0193a04400a83aaeec7eea2a1b828
ManyiZ <zhaomanyi2005@gmail.com>
2021-09-21 21:13:53 -0700

    pseudo code for 1 auto mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto1.java
@@ -17,5 +17,21 @@ public class Auto1 extends LinearOpMode implements FieldPositions {
     public void runOpMode()
     {
 
+        //Detect the barcode and place the game element to corresponding level
+
+        //Move to the carousal to deliver the duck
+
+        //Move to the side with the intake facing the warehouse
+
+        //Detect the weight of the freight if possible
+
+        //Deliver the freight to the team shipping hub
+
+        //Repeat the last two steps until there's 2-5 secs left on the auto timer
+
+        //Park in the warehouse
+
+
+
     }
 }
