73a5d9d1789f5684e0ae5641864c3f6e8f940ed5
Ben Armstrong <benden3697@gmail.com>
2021-09-24 16:54:29 -0700

    Fixed gradle build and added first draft image analysis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
@@ -29,6 +29,23 @@
 
 package org.firstinspires.ftc.teamcode.teleop;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.util.Size;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.lifecycle.LifecycleOwner;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -36,6 +53,13 @@ import org.firstinspires.ftc.teamcode.robot_components.input.Btn;
 import org.firstinspires.ftc.teamcode.data.FieldPositions;
 import org.firstinspires.ftc.teamcode.robot_components.input.Controller;
 import org.firstinspires.ftc.teamcode.robot_components.cv.CVModule;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @TeleOp(name="Tele1", group="Linear Opmode")
 public class Tele1 extends LinearOpMode implements FieldPositions {
@@ -88,7 +112,59 @@ public class Tele1 extends LinearOpMode implements FieldPositions {
                 );
             }
 
-            // this is my cool change
+            Thread initCam = new Thread() {
+                public void run() {
+                    ExecutorService executor = Executors.newSingleThreadExecutor();
+                    ImageAnalysis imageAnalysis =
+                            new ImageAnalysis.Builder()
+                                    .setTargetResolution(new Size(720, 1280))
+                                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                    .build();
+
+                    imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+                        public void analyze(ImageProxy imageProxy) {
+                            //int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
+                            // insert your code here.
+                            @SuppressLint("UnsafeExperimentalUsageError") Image image = imageProxy.getImage();
+                            Bitmap tempMap = toBitmap(image);
+                            Bitmap bitmap = Bitmap.createScaledBitmap(tempMap, 224, 224, true);
+                            ByteBuffer input = ByteBuffer.allocateDirect(224 * 224 * 3 * 4).order(ByteOrder.nativeOrder());
+                            for (int y = 0; y < 224; y++) {
+                                for (int x = 0; x < 224; x++) {
+                                    int px = bitmap.getPixel(x, y);
+
+                                    // Get channel values from the pixel value.
+                                    int r = Color.red(px);
+                                    int g = Color.green(px);
+                                    int b = Color.blue(px);
+
+                                    // Normalize channel values to [-1.0, 1.0]. This requirement depends
+                                    // on the model. For example, some models might require values to be
+                                    // normalized to the range [0.0, 1.0] instead.
+                                    float rf = (r - 127) / 255.0f;
+                                    float gf = (g - 127) / 255.0f;
+                                    float bf = (b - 127) / 255.0f;
+
+                                    input.putFloat(rf);
+                                    input.putFloat(gf);
+                                    input.putFloat(bf);
+                                }
+                            }
+                            int colour = tempMap.getPixel(0, 0);
+                            int red = Color.red(colour);
+                            int green = Color.green(colour);
+                            int blue = Color.blue(colour);
+                            int alpha = Color.alpha(colour);
+                            System.out.println(red + ", " + blue + ", " + green + ", " + alpha);
+                        };
+                    });
+
+                }
+            };
+
+            initCam.start();
+
+
 
             // -----------------------------------------------------------------------------------------
             // -----------------------------------------------------------------------------------------
@@ -99,5 +175,28 @@ public class Tele1 extends LinearOpMode implements FieldPositions {
             //OPERATOR FUNCTIONS
 
         }
+    }
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
     }
 }
