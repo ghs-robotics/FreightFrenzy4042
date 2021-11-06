453aa2843752f60b2c96d00f9c8bdf57d593baae
C34A <theo@seelye.net>
2021-11-05 19:10:08 -0700

    constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navigation/tasks/DriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navigation/tasks/DriveToPoint.java
@@ -5,10 +5,24 @@ import org.firstinspires.ftc.teamcode.navigation.RobotPosition;
 import org.firstinspires.ftc.teamcode.navigation.Task;
 import org.firstinspires.ftc.teamcode.robot_components.robot.Robot;
 
+/**
+ * Drive to a point on the field.
+ */
 public class DriveToPoint implements Task {
 
     public RobotPosition targetPosition;
     public RobotPosition errorMargin;
+
+    public DriveToPoint(RobotPosition targetPosition) {
+        // default error margin of 1.5cm, 10 degrees
+        this(targetPosition, new RobotPosition(15.0, 15.0, 10.0));
+    }
+
+    public DriveToPoint(RobotPosition targetPosition, RobotPosition errorMargin) {
+        this.targetPosition = targetPosition;
+        this.errorMargin = errorMargin;
+    }
+
 
     public void init() {
         // don't need to do anything
@@ -19,7 +33,7 @@ public class DriveToPoint implements Task {
         Point2D error = targetPosition.position.subtract(currentPosition.position);
         double rotError = targetPosition.rotation - currentPosition.rotation;
 
-        // todo: this is kinda jank and should use PID
+        // todo: this is kinda jank and should use PID or something
         robot.calculateDrivePowers(error.x, error.y, rotError);
 
         return arrived(currentPosition);
