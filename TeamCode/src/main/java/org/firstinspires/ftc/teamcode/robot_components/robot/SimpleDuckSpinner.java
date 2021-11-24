06f71391e759210b0149c9b96142f9627ee7f5d1
ewaite135 <ewaite135@gmail.com>
2021-11-23 17:08:35 -0800

    Replaced the DcMotor with CRServo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_components/robot/SimpleDuckSpinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_components/robot/SimpleDuckSpinner.java
@@ -3,7 +3,8 @@ package org.firstinspires.ftc.teamcode.robot_components.robot;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -11,49 +12,44 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import java.util.Optional;
 
 public class SimpleDuckSpinner {
-    DcMotor motor;
+    CRServo spinnerServo;
     private ElapsedTime runtime;
     private Telemetry telemetry;
     private boolean isRunning = false;
     //Should be adjusted experimentally
-    final double MOTOR_SPEED = 0.5;
+    final double SERVO_SPEED = 0.5;
     //O means not running, 1 means forward, and -1 means running backwards
-    private double motorDirection = 0;
+    private double servoDirection = 0;
     // TODO: SET DIRECTION !!!!
 
-    public SimpleDuckSpinner(DcMotor motor, double radius) {
-        this(motor, radius, null);
+    public SimpleDuckSpinner(CRServo spinnerServo, double radius) {
+        this(spinnerServo, radius, null);
     }
 
     public SimpleDuckSpinner(
-            DcMotor motor,
+            CRServo spinnerServo,
             double radius,
             Telemetry telemetry //,
-//            boolean useEncoders
     ){
-        this.motor = motor;
-        motor.setDirection(DcMotor.Direction.FORWARD);
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.spinnerServo = spinnerServo;
+        spinnerServo.setDirection(CRServo.Direction.FORWARD);
         this.runtime = new ElapsedTime();
         this.telemetry = telemetry;
-
-//        this.useEncoder = useEncoders;
     }
 
     public void startRunningForwards() {
         runtime.reset();
-        motorDirection = 1;
+        servoDirection = 1;
         isRunning = true;
     }
     public void startRunningBackwards() {
         runtime.reset();
-        motorDirection = -1;
+        servoDirection = -1;
         isRunning = true;
     }
     public void stopRunning() {
         runtime.reset();
-        motorDirection = 0;
+        servoDirection = 0;
         isRunning = false;
     }
     /**
@@ -64,9 +60,9 @@ public class SimpleDuckSpinner {
         double time = runtime.seconds();
         if(telemetry != null) {
             telemetry.addData("duck time: ", time);
-            telemetry.addData("duck motor power: ", motor.getPower());
-            telemetry.addData("Goal motor power: ", MOTOR_SPEED * motorDirection);
+            telemetry.addData("duck motor power: ", spinnerServo.getPower());
+            telemetry.addData("Goal motor power: ", SERVO_SPEED * servoDirection);
         }
-        motor.setPower(MOTOR_SPEED * motorDirection);
+        spinnerServo.setPower(SERVO_SPEED * servoDirection);
     }
 }
